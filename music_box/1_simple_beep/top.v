a61d663794ec811daa643e6e4018ca52d03c889c
Jim Pollock <jmpollock@google.com>
2020-10-14 18:15:11 +0100

    Update to use 440hz clock divider param

--- music_box/1_simple_beep/top.v
+++ music_box/1_simple_beep/top.v
@@ -1,38 +1,30 @@
 module top(
-    // ***(A)***
     input CLK100MHZ,
     output [3:0] jd,
     output [3:0] led,
     input [3:0] sw
     );
 
-// ***(B)***
-// first create an 18bit binary counter  
-reg [17:0] counter;
-always @(posedge CLK100MHZ) counter <= counter+1;
+reg [16:0] counter;
+reg speaker;
+parameter clkdivider = 100000000/440/2;  // This fits in 17 bits, hence the counter size
 
-// and use the most significant bit (MSB) of the counter to drive the speaker
-wire speaker_out = counter[17];
+always @(posedge CLK100MHZ) if(counter==0) counter <= clkdivider-1; else counter <= counter-1;
+always @(posedge CLK100MHZ) if(counter==0) speaker <= ~speaker;
 
-
-// ***(C)***
-// EITHER 
-// (1) you wish to annoy your neighbors, so send through the full speaker volume,  
-//assign jd[0] = speaker_out
-// OR 
-// (2) just send through the 1/64th of the signal by only sending signal when last 6 bits of counter are zero 
-assign jd[0] = speaker_out & (counter[6:0] == 0);
-
-// ***(D)***
+// A 7 bit counter is 0 once every 128 cycles.
+// At 100MHz, this is too fast to make a sonic difference,
+// but will average out the analog signal to a much lower
+// volume.
+assign jd[0] = speaker & (counter[6:0] == 0);
+// Set switch 0 to gain control. 1 is low gain, so lets make that default
+assign jd[1] = ~sw[0];
 // Set switch 3 to toggle shutdown pin, turning amplifier on and off.
-// If you have housemates/family at home, you almost certainly need this
 assign jd[3] = sw[3];
-   
-// ***(E)***
+
 // LEDs to help with debugging
-assign led[0] = speaker_out;   // Current wave form
-assign led[1] = jd[0];         // Attenuated signal sent to PMOD AMP
-assign led[3] = sw[3];
+assign led[0] = speaker;   // Current wave form
+assign led[1] = jd[0];     // Attenuated signal sent to PMOD AMP
+assign led[3] = sw[3];     // Sound on or off
 
 endmodule
-
